980ddd643c982bd560df6632efa2ff99d7fd6baf
harukitosa <13haruki28@gmail.com>
2020-08-17 23:14:22 +0900

    fix

--- src/components/Post/Content/Content.js
+++ src/components/Post/Content/Content.js
@@ -1,7 +1,12 @@
 // @flow strict
 import React from 'react';
 import styles from './Content.module.scss';
-
+import {
+  FacebookShareButton,
+  FacebookIcon,
+  TwitterShareButton,
+  TwitterIcon,
+} from 'react-share';
 type Props = {
   body: string,
   title: string
@@ -11,6 +16,14 @@ const Content = ({ body, title }: Props) => (
   <div className={styles['content']}>
     <h1 className={styles['content__title']}>{title}</h1>
     <div className={styles['content__body']} dangerouslySetInnerHTML={{ __html: body }} />
+    <div className={styles['share']}> 
+      <FacebookShareButton url={location.href} className={styles["mr-2"]}>
+          <FacebookIcon size={40} round />
+      </FacebookShareButton>
+      <TwitterShareButton url={location.href} className={styles["mr-2"]}>
+          <TwitterIcon size={40} round />
+      </TwitterShareButton>
+    </div>
   </div>
 );
 
